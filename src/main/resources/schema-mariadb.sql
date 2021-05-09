387eae010d09d48d93158459796aa5e0bea01e1c
cube8540 <cube8540@gmail.com>
2021-05-09 17:36:31 +0900

    출판사 테이블 수정

--- src/main/resources/schema-mariadb.sql
+++ src/main/resources/schema-mariadb.sql
@@ -162,6 +162,7 @@ create table if not exists division_raw_mappings (
 create table if not exists publishers (
     publisher_code varchar(32) not null primary key
 ) engine = InnoDB;
+alter table publishers add column if not exists name varchar(32);
 
 create table if not exists publisher_raw_mappings (
     publisher_code varchar(32) not null,
