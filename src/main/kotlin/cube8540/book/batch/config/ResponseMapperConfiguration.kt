dfbc237df8ba91cce83965809f0d2f5206408780
cube8540 <cube8540@gmail.com>
2021-05-16 03:27:13 +0900

    네이버 API XMl => JSON 변경

--- src/main/kotlin/cube8540/book/batch/config/ResponseMapperConfiguration.kt
+++ src/main/kotlin/cube8540/book/batch/config/ResponseMapperConfiguration.kt
@@ -2,10 +2,8 @@ package cube8540.book.batch.config
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.module.SimpleModule
-import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
@@ -19,7 +17,6 @@ import cube8540.book.batch.domain.repository.PublisherRepository
 import cube8540.book.batch.external.BookAPIErrorResponse
 import cube8540.book.batch.external.BookAPIResponse
 import cube8540.book.batch.infra.kyobo.kr.KyoboBookDocumentMapper
-import cube8540.book.batch.infra.naver.com.NaverAPIJsonNodeDeserializer
 import cube8540.book.batch.infra.naver.com.NaverBookAPIDeserializer
 import cube8540.book.batch.infra.naver.com.NaverBookAPIErrorDeserializer
 import cube8540.book.batch.infra.nl.go.NationalLibraryAPIDeserializer
@@ -76,10 +73,9 @@ class ResponseMapperConfiguration {
     @Bean
     fun naverBookAPIObjectMapper(
         @Qualifier("naverPublisherRawMapper") publisherRawMapper: PublisherRawMapper
-    ): ObjectMapper = XmlMapper()
+    ): ObjectMapper = ObjectMapper()
         .registerModule(
             SimpleModule()
-                .addDeserializer(JsonNode::class.java, NaverAPIJsonNodeDeserializer())
                 .addDeserializer(BookAPIResponse::class.java, NaverBookAPIDeserializer(publisherRawMapper))
                 .addDeserializer(BookAPIErrorResponse::class.java, NaverBookAPIErrorDeserializer())
         )
