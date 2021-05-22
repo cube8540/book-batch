6362404ebd7d92f0954fc11e08d99c39fccb2c72
cube8540 <cube8540@gmail.com>
2021-05-22 18:37:17 +0900

    도서 업로드 대상 잡 배치 설정 추가

---
+++ src/main/kotlin/cube8540/book/batch/job/BookSetUpstreamTargetJobConfiguration.kt
@@ -0,0 +1,112 @@
+package cube8540.book.batch.job
+
+import cube8540.book.batch.config.JobTaskExecutorProperty
+import cube8540.book.batch.domain.BookDetails
+import cube8540.book.batch.domain.BookDetailsFilterFunction
+import cube8540.book.batch.domain.repository.BookDetailsRepository
+import cube8540.book.batch.infra.BookDetailsFilterProcessor
+import cube8540.book.batch.infra.BookSetUpstreamTargetProcessor
+import cube8540.book.batch.infra.RepositoryBasedBookReader
+import cube8540.book.batch.infra.RepositoryBasedUpstreamTargetWriter
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.item.support.CompositeItemProcessor
+import org.springframework.batch.item.support.SynchronizedItemStreamReader
+import org.springframework.batch.item.support.SynchronizedItemStreamWriter
+import org.springframework.batch.item.support.builder.CompositeItemProcessorBuilder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.TaskExecutor
+
+@Configuration
+class BookSetUpstreamTargetJobConfiguration {
+
+    companion object {
+        const val jobName = "bookSetUpstreamTarget"
+        const val jobStepName = jobName + "Step"
+        const val jobReaderName = jobName + "JobReader"
+        const val jobProcessorName = jobName + "JobProcessor"
+        const val jobWriterName = jobName + "JobWriter"
+        const val defaultChunkSize = 1000
+    }
+
+    @set:Autowired
+    lateinit var jobBuilderFactory: JobBuilderFactory
+
+    @set:Autowired
+    lateinit var stepBuilderFactory: StepBuilderFactory
+
+    @set:Autowired
+    lateinit var jobTaskExecutorProperty: JobTaskExecutorProperty
+
+    @set:Autowired
+    lateinit var bookDetailsRepository: BookDetailsRepository
+
+    @set:Autowired
+    lateinit var jobParameter: BookAPIRequestJobParameter
+
+    @set:[Autowired Qualifier("jobTaskExecutor")]
+    lateinit var jobTaskExecutor: TaskExecutor
+
+    @set:[Autowired Qualifier("nationalLibraryFilterFunction")]
+    lateinit var nationalLibraryBookDetailsFilterFunction: BookDetailsFilterFunction
+
+    @set:[Autowired Qualifier("kyoboBookFilterFunction")]
+    lateinit var kyoboBookDetailsFilterFunction: BookDetailsFilterFunction
+
+    var chunkSize = defaultChunkSize
+
+    @Bean(jobName)
+    fun bookSetUpstreamJob(): Job = jobBuilderFactory.get(jobName)
+        .start(bookSetUpstreamJobStep())
+        .build()
+
+    @JobScope
+    @Bean(jobStepName)
+    fun bookSetUpstreamJobStep(): Step = stepBuilderFactory.get(jobStepName)
+        .chunk<BookDetails, BookDetails>(chunkSize)
+        .reader(bookDetailsReader())
+        .processor(bookDetailsProcessor())
+        .writer(bookDetailsWriter())
+        .taskExecutor(jobTaskExecutor)
+        .throttleLimit(jobTaskExecutorProperty.throttleLimit!!)
+        .build()
+
+    @StepScope
+    @Bean(jobReaderName)
+    fun bookDetailsReader(): SynchronizedItemStreamReader<BookDetails> {
+        val reader = RepositoryBasedBookReader(bookDetailsRepository, jobParameter.from!!, jobParameter.to!!)
+        reader.pageSize = chunkSize
+
+        val synchronizedItemStreamReader = SynchronizedItemStreamReader<BookDetails>()
+        synchronizedItemStreamReader.setDelegate(reader)
+        return synchronizedItemStreamReader
+    }
+
+    @StepScope
+    @Bean(jobProcessorName)
+    fun bookDetailsProcessor(): CompositeItemProcessor<BookDetails, BookDetails> =
+        CompositeItemProcessorBuilder<BookDetails, BookDetails>()
+            .delegates(
+                BookDetailsFilterProcessor(nationalLibraryBookDetailsFilterFunction),
+                BookDetailsFilterProcessor(kyoboBookDetailsFilterFunction),
+                BookSetUpstreamTargetProcessor()
+            )
+            .build()
+
+    @StepScope
+    @Bean(jobWriterName)
+    fun bookDetailsWriter(): SynchronizedItemStreamWriter<BookDetails> {
+        val writer = RepositoryBasedUpstreamTargetWriter(bookDetailsRepository)
+
+        val synchronizedItemStreamWriter = SynchronizedItemStreamWriter<BookDetails>()
+        synchronizedItemStreamWriter.setDelegate(writer)
+        return synchronizedItemStreamWriter
+    }
+}
---
+++ src/test/kotlin/cube8540/book/batch/job/BookSetUpstreamTargetTest.kt
@@ -0,0 +1,123 @@
+package cube8540.book.batch.job
+
+import com.nhaarman.mockitokotlin2.capture
+import cube8540.book.batch.domain.BookDetails
+import cube8540.book.batch.domain.BookDetailsFilterFunction
+import cube8540.book.batch.domain.repository.BookDetailsRepository
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mockito
+import org.springframework.batch.core.BatchStatus
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParameters
+import org.springframework.batch.core.JobParametersBuilder
+import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.batch.test.JobLauncherTestUtils
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestConstructor
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import kotlin.random.Random
+
+@SpringBootTest
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class BookSetUpstreamTargetTest constructor(
+    @Qualifier("bookSetUpstreamTarget")
+    private val job: Job,
+
+    jobLauncher: JobLauncher
+) {
+
+    @MockBean
+    lateinit var bookDetailsRepository: BookDetailsRepository
+
+    @MockBean
+    @Qualifier("nationalLibraryFilterFunction")
+    lateinit var nationalLibraryFilterFunction: BookDetailsFilterFunction
+
+    @MockBean
+    @Qualifier("kyoboBookFilterFunction")
+    lateinit var kyoboFilterFunction: BookDetailsFilterFunction
+
+    @Captor
+    lateinit var persistCaptor: ArgumentCaptor<Collection<BookDetails>>
+
+    private val jobLauncherTestUtils = JobLauncherTestUtils()
+
+    private val from = LocalDate.of(2021, 1, 1)
+    private val to = LocalDate.of(2021, 5, 31)
+
+    init {
+        jobLauncherTestUtils.job = job
+        jobLauncherTestUtils.jobLauncher = jobLauncher
+    }
+
+    @Test
+    fun `set book details upstream target`() {
+        val jobParameters = createJobParameters()
+        val bookDetails: BookDetails = mockk(relaxed = true)
+
+        val queryResultContents = listOf(bookDetails)
+        val firstPageRequest = PageRequest.of(0, BookSetUpstreamTargetJobConfiguration.defaultChunkSize)
+        val secondPageRequest = PageRequest.of(1, BookSetUpstreamTargetJobConfiguration.defaultChunkSize)
+
+        Mockito.`when`(bookDetailsRepository.findByPublishDateBetween(from, to, firstPageRequest))
+            .thenReturn(PageImpl(queryResultContents, firstPageRequest, 1))
+        Mockito.`when`(bookDetailsRepository.findByPublishDateBetween(from, to, secondPageRequest))
+            .thenReturn(PageImpl(emptyList(), secondPageRequest, 0))
+        Mockito.`when`(nationalLibraryFilterFunction.isValid(bookDetails)).thenReturn(true)
+        Mockito.`when`(kyoboFilterFunction.isValid(bookDetails)).thenReturn(true)
+
+        val jobExecution = jobLauncherTestUtils.launchJob(jobParameters)
+        Mockito.verify(bookDetailsRepository).updateForUpstreamTarget(capture(persistCaptor))
+
+        val upstreamTarget = persistCaptor.value.toList()
+        assertThat(jobExecution.status).isEqualTo(BatchStatus.COMPLETED)
+        assertThat(upstreamTarget[0]).isEqualTo(bookDetails)
+        verify {
+            bookDetails.isUpstreamTarget = true
+        }
+    }
+
+    @Test
+    fun `set book details upstream target when filtering returns false`() {
+        val jobParameters = createJobParameters()
+        val bookDetails: BookDetails = mockk(relaxed = true)
+
+        val queryResultContents = listOf(bookDetails)
+        val firstPageRequest = PageRequest.of(0, BookSetUpstreamTargetJobConfiguration.defaultChunkSize)
+        val secondPageRequest = PageRequest.of(1, BookSetUpstreamTargetJobConfiguration.defaultChunkSize)
+
+        Mockito.`when`(bookDetailsRepository.findByPublishDateBetween(from, to, firstPageRequest))
+            .thenReturn(PageImpl(queryResultContents, firstPageRequest, 1))
+        Mockito.`when`(bookDetailsRepository.findByPublishDateBetween(from, to, secondPageRequest))
+            .thenReturn(PageImpl(emptyList(), secondPageRequest, 0))
+        Mockito.`when`(nationalLibraryFilterFunction.isValid(bookDetails)).thenReturn(false)
+        Mockito.`when`(kyoboFilterFunction.isValid(bookDetails)).thenReturn(true)
+
+        val jobExecution = jobLauncherTestUtils.launchJob(jobParameters)
+        assertThat(jobExecution.status).isEqualTo(BatchStatus.COMPLETED)
+        Mockito.verify(bookDetailsRepository).updateForUpstreamTarget(emptyList())
+    }
+
+    private fun createJobParameters(): JobParameters =
+        JobParametersBuilder()
+            .addString("from", KyoboBookRequestJobTestEnvironment.from.format(DateTimeFormatter.BASIC_ISO_DATE))
+            .addString("to", KyoboBookRequestJobTestEnvironment.to.format(DateTimeFormatter.BASIC_ISO_DATE))
+            .addLong("seed", Random.nextLong())
+            .toJobParameters()
+}
