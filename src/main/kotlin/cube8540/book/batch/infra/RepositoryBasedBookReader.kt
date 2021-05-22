2f4579c9b1a5ff18d2d7f3505f50aaf59238bee2
cube8540 <cube8540@gmail.com>
2021-05-23 00:45:50 +0900

    RepositoryBasedBookReader detached 호출 추가

--- src/main/kotlin/cube8540/book/batch/infra/RepositoryBasedBookReader.kt
+++ src/main/kotlin/cube8540/book/batch/infra/RepositoryBasedBookReader.kt
@@ -1,9 +1,11 @@
 package cube8540.book.batch.infra
 
 import cube8540.book.batch.domain.BookDetails
+import cube8540.book.batch.domain.QBookDetails
 import cube8540.book.batch.domain.repository.BookDetailsRepository
 import org.springframework.batch.item.database.AbstractPagingItemReader
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import java.time.LocalDate
 
 open class RepositoryBasedBookReader(
@@ -20,7 +22,9 @@ open class RepositoryBasedBookReader(
             results.clear()
         }
 
-        val bookDetailsPage = bookDetailsRepository.findByPublishDateBetween(from, to, PageRequest.of(page, pageSize))
+        val sort = Sort.by(Sort.Order.desc(QBookDetails.bookDetails.publishDate.metadata.name))
+        val bookDetailsPage = bookDetailsRepository.findByPublishDateBetween(from, to, PageRequest.of(page, pageSize, sort))
+        bookDetailsRepository.detached(bookDetailsPage.content)
         results.addAll(bookDetailsPage.content)
     }
 
