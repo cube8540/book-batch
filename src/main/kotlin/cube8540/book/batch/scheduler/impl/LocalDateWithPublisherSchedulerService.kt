73aca761c7cafd0004db354640e32688c2eb1f97
cube8540 <cube8540@gmail.com>
2021-05-23 16:01:14 +0900

    스케줄러 추가

---
+++ src/main/kotlin/cube8540/book/batch/scheduler/impl/LocalDateWithPublisherSchedulerService.kt
@@ -0,0 +1,43 @@
+package cube8540.book.batch.scheduler.impl
+
+import cube8540.book.batch.BatchApplication
+import cube8540.book.batch.domain.MappingType
+import cube8540.book.batch.domain.repository.PublisherRepository
+import cube8540.book.batch.job.JobParameterNames
+import cube8540.book.batch.scheduler.JobSchedulerService
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParametersBuilder
+import org.springframework.batch.core.launch.JobLauncher
+import java.time.Clock
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class LocalDateWithPublisherSchedulerService(
+    private val mappingType: MappingType,
+    private val publisherRepository: PublisherRepository
+): JobSchedulerService {
+
+    companion object {
+        internal var clock = Clock.system(BatchApplication.DEFAULT_TIME_ZONE.toZoneId())
+    }
+
+    lateinit var job: Job
+
+    lateinit var jobLauncher: JobLauncher
+
+    override fun launchBookDetailsRequest(from: LocalDate, to: LocalDate) {
+        val keywords = publisherRepository
+            .findByMappingTypeWithKeyword(mappingType).flatMap { it.keywords }.map { it.raw }
+
+        keywords.forEach {
+            val jobParameter = JobParametersBuilder()
+                .addString(JobParameterNames.from, from.format(DateTimeFormatter.BASIC_ISO_DATE))
+                .addString(JobParameterNames.to, to.format(DateTimeFormatter.BASIC_ISO_DATE))
+                .addString(JobParameterNames.publisher, it)
+                .addString(JobParameterNames.startup, LocalDateTime.now(clock).format(DateTimeFormatter.ISO_DATE_TIME))
+                .toJobParameters()
+            jobLauncher.run(job, jobParameter)
+        }
+    }
+}
---
+++ src/test/kotlin/cube8540/book/batch/scheduler/impl/LocalDateJobSchedulerServiceTest.kt
@@ -0,0 +1,45 @@
+package cube8540.book.batch.scheduler.impl
+
+import cube8540.book.batch.BatchApplication
+import cube8540.book.batch.job.JobParameterNames
+import cube8540.book.batch.toDefaultInstance
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParametersBuilder
+import org.springframework.batch.core.launch.JobLauncher
+import java.time.Clock
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class LocalDateJobSchedulerServiceTest {
+
+    private val from = LocalDate.of(2021, 1, 1)
+    private val to = LocalDate.of(2021, 5, 31)
+
+    private val now = LocalDateTime.of(2021, 5, 23, 13, 33, 0)
+
+    private val job: Job = mockk(relaxed = true)
+    private val jobLauncher: JobLauncher = mockk(relaxed = true)
+
+    private val service = LocalDateJobSchedulerService(job, jobLauncher)
+
+    init {
+        LocalDateJobSchedulerService.clock = Clock.fixed(now.toDefaultInstance(), BatchApplication.DEFAULT_TIME_ZONE.toZoneId())
+    }
+
+    @Test
+    fun `job launched`() {
+        service.launchBookDetailsRequest(from, to)
+        verify { jobLauncher.run(job,  createExceptedJobParameter(from, to)) }
+    }
+
+    private fun createExceptedJobParameter(from: LocalDate, to: LocalDate) = JobParametersBuilder()
+        .addString(JobParameterNames.from, from.format(DateTimeFormatter.BASIC_ISO_DATE))
+        .addString(JobParameterNames.to, to.format(DateTimeFormatter.BASIC_ISO_DATE))
+        .addString(JobParameterNames.startup, now.format(DateTimeFormatter.ISO_DATE_TIME))
+        .toJobParameters()
+
+}
