c191062a5961eff5a0d98e08c4751b2448027b71
cube8540 <cube8540@gmail.com>
2022-06-18 04:20:40 +0900

    깃 리베이스

--- src/main/kotlin/cube8540/book/batch/translator/naver/com/client/NaverBookClientError.kt
+++
@@ -1,7 +0,0 @@
-package cube8540.book.batch.translator.naver.com.client
-
-import java.beans.ConstructorProperties
-
-data class NaverBookClientError
-@ConstructorProperties(value = ["errorMessage", "errorCode"])
-constructor(var errorMessage: String, var errorCode: String)
