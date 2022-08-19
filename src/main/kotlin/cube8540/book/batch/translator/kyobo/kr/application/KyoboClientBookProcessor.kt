6339347676aa3ea7ceb4b5154970c0bee6915bc5
cube8540 <cube8540@gmail.com>
2022-08-20 01:48:36 +0900

    교보문고 도서를 찾을 수 없어 에러가 발생 했을때 해당 도서는 무시 하도록 수정

--- src/main/kotlin/cube8540/book/batch/translator/kyobo/kr/application/KyoboClientBookProcessor.kt
+++ src/main/kotlin/cube8540/book/batch/translator/kyobo/kr/application/KyoboClientBookProcessor.kt
@@ -2,9 +2,12 @@ package cube8540.book.batch.translator.kyobo.kr.application
 
 import cube8540.book.batch.book.domain.BookDetails
 import cube8540.book.batch.book.domain.BookDetailsController
+import cube8540.book.batch.translator.client.InternalBadRequestException
+import cube8540.book.batch.translator.client.InvalidAuthenticationException
 import cube8540.book.batch.translator.kyobo.kr.client.KyoboBookClient
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.slf4j.LoggerFactory
 import org.springframework.batch.item.ItemProcessor
 
 class KyoboClientBookProcessor(
@@ -12,15 +15,26 @@ class KyoboClientBookProcessor(
     private val documentMapper: BookDocumentMapper
 ): ItemProcessor<BookDetails, BookDetails> {
 
+    private val logger = LoggerFactory.getLogger(KyoboClientBookProcessor::class.java)
+
     var controller: BookDetailsController = KyoboBookDetailsController()
 
-    override fun process(item: BookDetails): BookDetails {
+    override fun process(item: BookDetails): BookDetails? {
         val result = client.search(item.isbn)
 
-        val document = Jsoup.parse(result)
-            .outputSettings(Document.OutputSettings().prettyPrint(false))
+        return try {
+            val document = Jsoup.parse(result)
+                .outputSettings(Document.OutputSettings().prettyPrint(false))
 
-        val documentContext = documentMapper.convertValue(document)
-        return controller.merge(item, BookDetails(documentContext))
+            val documentContext = documentMapper.convertValue(document)
+            controller.merge(item, BookDetails(documentContext))
+        } catch (e: Exception) {
+            when (e) {
+                is InternalBadRequestException, is InvalidAuthenticationException -> {
+                    logger.error("Throws exception during convert book (${item.isbn})", e)
+                    null
+                } else -> throw e
+            }
+        }
     }
 }
